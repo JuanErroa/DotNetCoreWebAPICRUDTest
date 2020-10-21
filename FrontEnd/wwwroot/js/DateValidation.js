4f7a69f22002838f773b570892285eae6170957b
JuanErroa <JuanErroa@DESKTOP-VH98SF8>
2020-10-21 12:37:52 -0600

    Added DateValidation to Create and Edit jobs

---
+++ FrontEnd/wwwroot/js/DateValidation.js
@@ -0,0 +1,15 @@
+﻿$('form').on('submit', function (e) {
+    let createdAt = moment($('#datetimepicker1 input').val(), "DD/MM/YYYY");
+    let expiresAt = moment($('#datetimepicker2 input').val(), "DD/MM/YYYY");
+
+    if (!expiresAt.isAfter(createdAt)) {
+        e.preventDefault();
+        Swal.fire({
+            icon: 'error',
+            title: 'Expire date would be greater than Creation date',
+            showConfirmButton: false,
+            timer: 1500
+        });
+    }
+    
+});
